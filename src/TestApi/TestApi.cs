b5a0c09c533117534a39ee9fd75754ef5fcf2890
Mike Gore <mikegore1000@msn.com>
2017-03-29 03:53:01 +0100

    Initial commit

---
+++ src/TestApi/TestApi.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using System.Fabric;
+using System.Fabric.Health;
+using Microsoft.ServiceFabric.Services.Communication.Runtime;
+using Microsoft.ServiceFabric.Services.Runtime;
+
+namespace TestApi
+{
+    /// <summary>
+    /// The FabricRuntime creates an instance of this class for each service type instance. 
+    /// </summary>
+    internal sealed class TestApi : StatelessService
+    {
+        public TestApi(StatelessServiceContext context)
+            : base(context)
+        { }
+
+        /// <summary>
+        /// Optional override to create listeners (like tcp, http) for this service instance.
+        /// </summary>
+        /// <returns>The collection of listeners.</returns>
+        protected override IEnumerable<ServiceInstanceListener> CreateServiceInstanceListeners()
+        {
+            //this.Partition.ReportInstanceHealth(new HealthInformation("TEST-TestApi", "Default", HealthState.Error));
+
+            return new ServiceInstanceListener[]
+            {
+                new ServiceInstanceListener(serviceContext => new OwinCommunicationListener(Startup.ConfigureApp, serviceContext, "ServiceEndpoint"))
+            };
+        }
+    }
+}
